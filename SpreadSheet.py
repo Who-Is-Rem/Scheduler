b9e32d57e950f9ad345c48cec84de9116dd611ea
Who-Is-Rem <alvin.lee72442@gmail.com>
2023-10-26 17:24:30 -0400

    Altered Grid and created SpreadSheet

--- SpreadSheet.py
+++ SpreadSheet.py
@@ -2,11 +2,10 @@ from tkinter import *
 from tkinter import ttk
 
 """
-A class taht creates a spread sheet frame, should be equally sized grids 
+A class that creates a spread sheet frame, should be equally sized grids 
 in both direction and fill the entire parent frame, which would be the 
 root window when testing.
 """
-
 class Grid(ttk.Frame):
     """
     Should initialize with m rows and n columns. Columns and rows DO NOT represent 
@@ -22,29 +21,73 @@ class Grid(ttk.Frame):
         assert cols >= 1 and rows >= 1
 
         # ===== Initialize basic atributes =====
-        self.cols = cols
-        self.rows = rows
-        self.grid_cols = (cols*2)+1
-        self.grid_rows = (rows*2)+1
+        self.cols = IntVar(value=cols)
+        self.rows = IntVar(value=rows)
+        self.colSpan = (cols*2)+1
+        self.rowSpan = (rows*2)+1
 
 
         # ===== Initialize basic grid configs =====
         ttk.Frame.__init__(self, parent)   
-        self.grid_columnconfigure(list(range(1,self.grid_cols, 2)), uniform="cols",weight=1)
-        self.grid_rowconfigure(list(range(1, self.grid_rows, 2)), uniform="rows",weight=1)
+        self.grid_columnconfigure(list(range(1,self.getColSpan(), 2)), uniform="cols",weight=1)
+        self.grid_rowconfigure(list(range(1, self.getRowSpan(), 2)), uniform="rows",weight=1)
 
         # ===== Create the grid =====
         for i in range(cols+1):
-            sep = ttk.Separator(self, orient="vertical")
-            sep.grid(row=0, column=(i*2), rowspan=self.grid_rows, sticky="nsew")
-            self.bind("<Configure>", lambda sep=sep: sep.grid(row=0, column=(i*2), rowspan=self.getRowSpan(), sticky="nsew"), add="+")
+            self.makeColSep(i)
         for i in range(rows+1):
-            sep = ttk.Separator(self, orient="horizontal")
-            sep.grid(row=(i*2), column=0, columnspan=self.grid_cols, sticky="nsew")
-            self.bind("<Configure>", lambda sep=sep: sep.grid(row=(i*2), column=0, columnspan=self.getColSpan(), sticky="nsew"), add="+")
+            self.makeRowSep(i)
+
+    """
+    Has a custom event to detect when the number of rows are changed
+    """
+    def makeColSep(self, c):
+        sep = ttk.Separator(self, orient="vertical")
+        sep.grid(row=0, column=(c*2), rowspan=self.getRowSpan(), sticky="nsew")
+        self.bind("<<RowChange>>", lambda e, sep=sep: sep.grid(row=0, column=(c*2), rowspan=self.getRowSpan(), sticky="nsew"), add="+")
+
+    """
+    Has a custom event to detect when the number of columns are changed
+    """
+    def makeRowSep(self, r):
+        sep = ttk.Separator(self, orient="horizontal")
+        sep.grid(row=(r*2), column=0, columnspan=self.getColSpan(), sticky="nsew")
+        self.bind("<<ColumnChange>>", lambda e, sep=sep: sep.grid(row=(r*2), column=0, columnspan=self.getColSpan(), sticky="nsew"), add="+")
 
     def getColSpan(self):
-        return self.grid_cols
+        return (self.cols.get()*2)+1
     
     def getRowSpan(self):
-        return self.grid_rows
+        return (self.rows.get()*2)+1
+
+"""
+A subclass of Grid that can accept a command to add or remove a row/column
+"""
+class SpreadSheet(Grid):
+    def __init__(self, parent, cols, rows):
+        super().__init__(parent, cols, rows)
+
+        # ===== Trace Functionality =====
+        self.cols.trace("w", lambda *args: [
+            self.grid_columnconfigure(list(range(1,self.getColSpan(), 2)), uniform="cols",weight=1),
+            self.makeColSep(self.cols.get()),
+            self.event_generate("<<ColumnChange>>")
+            ])
+        self.rows.trace("w", lambda *args: [
+            self.grid_rowconfigure(list(range(1,self.getRowSpan(), 2)), uniform="rows",weight=1),
+            self.makeRowSep(self.rows.get()),
+            self.event_generate("<<RowChange>>")
+            ])
+
+    """
+    Simply add 1 to self.cols and let a IntVar.trace handle expansion
+    """
+    def add_column(self):
+        self.cols.set(self.cols.get()+1)
+
+    """
+    Simply add 1 to self.rows and let a IntVar.trace handle expansion
+    """
+    def add_row(self):
+        self.rows.set(self.rows.get()+1)
+        
