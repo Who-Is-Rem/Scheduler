b3e471893a517328235e76c4d39ed238ef7f2cfc
Who-Is-Rem <alvin.lee72442@gmail.com>
2023-10-29 22:58:16 -0400

    CustomerFrame partially dev. and mod. Service.py

---
+++ CustomerFrame.py
@@ -0,0 +1,89 @@
+from tkinter import *
+from tkinter import ttk
+from People import *
+from Service import *
+from CustomTime import *
+
+"""
+Create a basic draggable frame
+"""
+class DnD(ttk.Frame):
+    def __init__(self, parent, width=100, height=100):
+        # Make the parent the root window so we can freely change between parents
+        ttk.Frame.__init__(self, parent, width=width, height=height, style="Customer.TFrame")
+        self.grid_propagate(False)
+        self.bind_drag()
+
+    def bind_drag(self):
+        self.bind("<ButtonPress-1>", self.click)
+        self.bind("<B1-Motion>", self.motion)
+
+    def click(self, event):
+        widget = event.widget
+        widget._drag_start_x = event.x
+        widget._drag_start_y = event.y
+
+    def motion(self, event):
+        widget = event.widget
+        x = widget.winfo_x() - widget._drag_start_x + event.x
+        y = widget.winfo_y() - widget._drag_start_y + event.y
+        widget.place(x=x, y=y)
+
+"""
+Class that should support being gridded into a grid in the parent frame
+
+The initial parent frame of thw dnd widget should ALWAYS be the root
+This is to facilitate changing parent frame of the dragged widget
+
+Once the widget is gridded, the size of the widget should change to match 
+the size of the grid
+"""
+class DnDGrid(DnD):
+    def __init__(self, parent):
+        super().__init__(parent.winfo_toplevel())
+        self.parent = parent
+        self.bind("<Map>", lambda e: self.configure(width=self.winfo_width(), height=self.winfo_height()))
+
+    def bind_drag(self):
+        super().bind_drag()
+        self.bind("<B1-ButtonRelease>", self.drop)
+
+    def drop(self, event):
+        widget = event.widget
+        x, y = self.parent.grid_location(widget.winfo_x()+widget.winfo_width()//2,widget.winfo_y()-35)
+        xmax, ymax = self.parent.grid_size()
+        if x>=0 and y>=0 and x<xmax-1 and y<ymax-1:
+            self.grid(in_=self.parent,row=y, column=x, sticky="nsew")
+
+"""
+Should have a customer and alist of services as inputs
+"""
+class CustomerFrameBasic(DnDGrid):
+    def __init__(self, parent, queue, customer, services):
+        super().__init__(parent)
+        self.configure()
+        assert isinstance(customer, Customer)
+        abbrev = ""
+        for serv in services: 
+            assert isinstance(serv, Service)
+            abbrev += f"{serv.getShort()} "
+
+        self.price, self.time = services[0].addAll(services[1:]) if len(services)>1 else services[0].getPrice(), services[0].time_range
+        customer_label = Label(self, text=f"{customer.getFirstName()}")
+        services_label = Label(self, text=abbrev, wraplength=self.winfo_width(), anchor=NW)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        customer_label.grid(row=0, column=0, sticky="nsew")
+        ttk.Separator(self, orient=HORIZONTAL).grid(row=1, column=0, sticky="nsew")
+        services_label.grid(row=2, column=0, sticky="nsew")
+
+    def drop(self, event):
+        widget = event.widget
+        x, y = self.parent.grid_location(widget.winfo_x()+widget.winfo_width()//2,widget.winfo_y()-35)
+        xmax, ymax = self.parent.grid_size()
+        if x>=0 and y>=0 and x<xmax-1 and y<ymax-1:
+            self.grid(in_=self.parent,row=y, column=x, sticky="nsew")
+        
+        
--- Service.py
+++ Service.py
@@ -6,16 +6,20 @@ Class representing a Service.
 Should expect strings as the input for the initializer
 """
 class Service():
-    def __init__(self, name, price, esttime, maxtime):
+    def __init__(self, name, price, esttime, maxtime, short):
         assert isinstance(name, str) and isinstance(esttime, str) and isinstance(maxtime, str)
         assert isinstance(int(price), int)
         self.name = " ".join(list(map(lambda s: s[0].upper()+s[1:], name.split(" "))))
         self.price = int(price)
         self.time_range = ServiceTimeRange(esttime, maxtime)
+        self.short = short
 
     def __eq__(self, other):
         assert isinstance(other, Service)
         return self.toString()==other.toString()
+    
+    def getShort(self):
+        return self.short
 
     def toString(self):
         return self.name
@@ -31,3 +35,12 @@ class Service():
     
     def getDifferenceTime(self):
         return self.time_range.getDifference().toString()
+    
+    def addAll(self, *args):
+        if isinstance(args[0], list): 
+            args = args[0]
+            for service in args: assert isinstance(service, Service)
+        total_time = self.time_range.addAll(args)
+        total_price = 0
+        for service in args: total_price += service.getPrice()
+        return total_price, total_time
